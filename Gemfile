671c407e106635f6a1b76b3ecea3f40715467bee
Ole Michaelis <Ole.Michaelis@googlemail.com>
2015-07-20 20:26:39 +0200

    info page

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
+  gem 'pry-byebug'
 
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
@@ -47,3 +48,4 @@ gem "therubyracer"
 gem "less-rails" #Sprockets (what Rails 3.1 uses for its asset pipeline) supports LESS
 gem "twitter-bootstrap-rails"
 gem "twitter-bootstrap-rails"
+gem 'pry'
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,25 @@
+require 'open-uri'
+
 class WelcomeController < ApplicationController
   def index
+    day = Date::DAYNAMES[Date.new.day].downcase
+    schedule = YAML.load_file("#{Rails.root.to_s}/config/schedule.yml")
+    day_schedule = schedule[day] || schedule['thursday']
+    @now = false
+    if day_schedule
+      now = Time.now.getutc.to_i
+      day_schedule.each do |time, v|
+        next if now > time
+        if @now == false
+          @now = v
+        else
+          @next = v
+          break
+        end
+      end
+    else
+      @now = 'nothing'
+      @next = 'nothing'
+    end
   end
 end
